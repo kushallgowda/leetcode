76705af360a28b13b6c559d563cc0638bd23281f
kushallgowda <92008614+kushallgowda@users.noreply.github.com>
2025-04-28 16:01:35 +0530

    Time: 27 ms (27.28%), Space: 13.4 MB (39.92%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -2,16 +2,6 @@ class Solution(object):
     def isAnagram(self, s, t):
         if len(s) != len(t):
             return False
-        countS, countT = {}, {}
-
-        for i in range(len(s)): #building the hashmaps
-             #we know both the lens are same
-             countS[s[i]] = 1 + countS.get(s[i], 0) #zero is the default value when its not same 
-             countT[t[i]] = 1 + countT.get(t[i], 0)
-    
-        for c in countS: #iterating with keys 
-            if countS[c] != countT.get(c,0):
-                return False
-        return True
+        return sorted(s) == sorted(t)
 
         
